8043c10a2811db2cb5d20680c30885d0ee1c5afb
Philip Vella <philip.v.vella@gmail.com>
2021-08-11 11:57:18 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+(function () {
+    let title = document.getElementsByTagName("title")[0].text;
+    let prId = title.substring(
+        title.lastIndexOf("#") + 1, 
+        title.lastIndexOf(":")
+    );
+    let jiraId = document.getElementsByClassName('jira-issue-link')[0].text;
+
+    function CopyMe(TextToCopy) {
+      var TempText = document.createElement("input");
+      TempText.value = TextToCopy;
+      document.body.appendChild(TempText);
+      TempText.select();
+      
+      document.execCommand("copy");
+      document.body.removeChild(TempText);
+      
+      alert("Copied the text: " + TempText.value);
+    }
+
+    CopyMe(prId + " " + jiraId)
+})();
